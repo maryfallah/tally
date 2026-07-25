8b1aa4be68e1e07a8e9f28250a00722a3acbb182
maryfallah <maryam.fallah2712@gmail.com>
2026-07-25 21:41:57 +0200

    Add category selection

--- lib/widgets/category_grid.dart
+++ lib/widgets/category_grid.dart
@@ -2,16 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:tally/models/category.dart';
 import 'package:tally/widgets/category_item.dart';
 
-class CategoryGrid extends StatelessWidget {
+class CategoryGrid extends StatefulWidget {
   const CategoryGrid({super.key});
 
+  @override
+  State<CategoryGrid> createState() => _CategoryGridState();
+}
+
+class _CategoryGridState extends State<CategoryGrid> {
+  Category? selectedCategory;
   @override
   Widget build(BuildContext context) {
     return Wrap(
       spacing: 16,
       runSpacing: 16,
       children: Category.values.map((category) {
-        return CategoryItem(category: category);
+        return CategoryItem(
+          category: category,
+          isSelected: category == selectedCategory,
+          onTap: () {
+            setState(() {
+              selectedCategory = category;
+            });
+          },
+        );
       }).toList(),
     );
   }
--- lib/widgets/category_item.dart
+++ lib/widgets/category_item.dart
@@ -3,21 +3,48 @@ import 'package:tally/extensions/category_extension.dart';
 import 'package:tally/models/category.dart';
 
 class CategoryItem extends StatelessWidget {
-  const CategoryItem({super.key, required this.category});
+  const CategoryItem({
+    super.key,
+    required this.category,
+    required this.isSelected,
+    required this.onTap,
+  });
 
   final Category category;
+  final bool isSelected;
+  final VoidCallback onTap;
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: 95,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Icon(category.icon),
-          const SizedBox(height: 6),
-          Text(category.name, textAlign: TextAlign.center),
-        ],
+      child: InkWell(
+        onTap: onTap,
+        borderRadius: BorderRadius.circular(12),
+        child: Container(
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 6),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            border: Border.all(
+              color: isSelected
+                  ? Theme.of(context).colorScheme.primary
+                  : Colors.transparent, // or Colors.grey.shade300
+              width: 2,
+            ),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(category.icon),
+              const SizedBox(height: 6),
+              Text(
+                category.name,
+                textAlign: TextAlign.center,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
